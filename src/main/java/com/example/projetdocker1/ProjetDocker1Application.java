79ab5726a9bdd54fe7a7c31a7b805e12c22899f0
philippe.guerrier <137076130+VaschPG@users.noreply.github.com>
2024-01-25 18:25:12 -0500

    Premier commit

---
+++ src/main/java/com/example/projetdocker1/ProjetDocker1Application.java
@@ -0,0 +1,13 @@
+package com.example.projetdocker1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDocker1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDocker1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetdocker1/ProjetDocker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetdocker1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDocker1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
